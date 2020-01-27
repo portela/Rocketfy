1cde198f9c2cd6cad076d1061b95a94513f1213e
Henrique Portela <henrique.portela@gmail.com>
2020-01-26 22:58:23 -0300

    Not working ... too tired, going to bed.

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { loadLists } from '../../services/api';
+
+import BoardContext from './context';
 
 import { Container } from './styles';
 import List from '../List';
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(from, to) {
+    console.log(from, to);
+  }
+
   return (
-    <Container>
-      {lists.map(list => (
-        <List key={list.title} data={list} />
-      ))}
-    </Container>
+    <BoardContext.Proviver value={{ lists, move }}>
+      <Container>
+        {lists.map(list => (
+          <List key={list.title} data={list} />
+        ))}
+      </Container>
+    </BoardContext.Proviver>
   );
 }
