9e0337b2e8dfa1fa3cf4368ebb14201dd1aedf51
Henrique Portela <henrique.portela@gmail.com>
2020-02-22 10:27:30 -0300

    Remove react-dnd.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { DndProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
 
 import GlobalStyle from './styles/global';
 
@@ -9,12 +7,12 @@ import Board from './components/Board';
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
+    <>
       <Header />
       <Board />
 
       <GlobalStyle />
-    </DndProvider>
+    </>
   );
 }
 
